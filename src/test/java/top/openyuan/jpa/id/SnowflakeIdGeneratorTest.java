a4bce6e81053d0ff39a34f84367ae8f8971b8899
liangzhengyuan <2438150729@qq.com>
2022-03-10 23:19:01 +0800

    fs-sad～～sad～～sad

--- src/test/java/top/openyuan/jpa/id/SnowflakeIdGeneratorTest.java
+++ src/test/java/top/openyuan/jpa/id/SnowflakeIdGeneratorTest.java
@@ -1,10 +1,10 @@
 package top.openyuan.jpa.id;
 
 import org.junit.Test;
-import top.openyuan.jpa.core.incrementer.SnowflakeIdGenerator;
+import top.openyuan.jpa.core.incrementer.DefaultIdentifierGenerator;
 
 /**
- * {@link SnowflakeIdGenerator} 的单元测试类.
+ * {@link DefaultIdentifierGenerator} 的单元测试类.
  *
  * @author lzy
  * @since v1.0.0
@@ -13,7 +13,7 @@ public class SnowflakeIdGeneratorTest {
 
     @Test
     public void generate() {
-        Long id = (Long) new SnowflakeIdGenerator().generate(null, null);
+        Long id = (Long) new DefaultIdentifierGenerator().generate(null, null);
         System.out.println(id);
     }
 }
